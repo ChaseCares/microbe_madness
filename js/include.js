2fb11273faef5ba6762471fd3fc3cfd40349aa59
Chase C <13863948+ChaseCares@users.noreply.github.com>
2022-11-17 12:45:55 -0500

    add previous bracket buttons

--- js/include.js
+++ js/include.js
@@ -66,6 +66,36 @@ var bracket = 'am';
 var seed = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 
 
+//*** This code is copyright 2002-2016 by Gavin Kistner, !@phrogz.net
+//*** It is covered under the license viewable at http://phrogz.net/JS/_ReuseLicense.txt
+//*** Reuse or modification is free provided you abide by the terms of that license.
+//*** (Including the first two lines above in your source code satisfies the conditions.)
+
+// Include this code (with notice above ;) in your library; read below for how to use it.
+
+Date.prototype.customFormat = function(formatString){
+	var YYYY,YY,MMMM,MMM,MM,M,DDDD,DDD,DD,D,hhhh,hhh,hh,h,mm,m,ss,s,ampm,AMPM,dMod,th;
+	var dateObject = this;
+	YY = ((YYYY=dateObject.getFullYear())+"").slice(-2);
+	MM = (M=dateObject.getMonth()+1)<10?('0'+M):M;
+	MMM = (MMMM=["January","February","March","April","May","June","July","August","September","October","November","December"][M-1]).substring(0,3);
+	DD = (D=dateObject.getDate())<10?('0'+D):D;
+	DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][dateObject.getDay()]).substring(0,3);
+	th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
+	formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#th#",th);
+
+	h=(hhh=dateObject.getHours());
+	if (h==0) h=24;
+	if (h>12) h-=12;
+	hh = h<10?('0'+h):h;
+    hhhh = hhh<10?('0'+hhh):hhh;
+	AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
+	mm=(m=dateObject.getMinutes())<10?('0'+m):m;
+	ss=(s=dateObject.getSeconds())<10?('0'+s):s;
+	return formatString.replace("#hhhh#",hhhh).replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
+};
+
+
 function getDataFromURL() {
     const url = window.location.href;
     root_url = url.split('?')[0];
@@ -150,11 +180,7 @@ function addWinner(from, to, index) {
 
 function toggleBracket(value) {
     bracket = value;
-    AM_BRACKET_TITLE.classList.toggle('hidden');
-    PM_BRACKET_TITLE.classList.toggle('hidden');
-    GRID.classList.toggle('am');
-    GRID.classList.toggle('pm');
-    setURL(bracket, seed);
+    setBracket();
 }
 
 
@@ -258,7 +284,14 @@ function saveBracket() {
 }
 
 function addBracketsToPageHelper(bracket) {
-    PREVIOUS_BRACKETS.innerHTML += `<input type="button" value="Load ${localStorage.getItem(bracket).split(',')[0]}, ${Array.from(localStorage.getItem(bracket).split(',')[1])[0]}'s ${bracket.toUpperCase()} bracket" onclick="loadBracket('${bracket}')">`;
+    const data = localStorage.getItem(bracket).split(',');
+    const fName = data[0];
+    const lName = data[1];
+    const displayBracket = bracket.split('-')[0].toUpperCase();
+    const date = new Date(Number(data[4]));
+    const displayDate = date.customFormat("#DDD# #MMM# #DD# #YYYY# #hh#:#mm# #AMPM#");
+
+    PREVIOUS_BRACKETS.innerHTML += `<input type="button" value="Load ${fName}, ${Array.from(lName)[0]}'s ${displayBracket} bracket, submitted on ${displayDate}" onclick="loadBracket('${bracket}')">`;
 }
 
 
@@ -267,11 +300,11 @@ function loadBracket(LocBracket) {
     FIRST_NAME.value = LOC_BRACKET_DATA[0];
     OVERALL_WINNER.value = LOC_BRACKET_DATA[2];
     seed = atob(LOC_BRACKET_DATA[3]).split(',').map(Number);
-    bracket = LocBracket;
-    console.log(LocBracket.split('-')[0]);
-    setURL(LocBracket.split('-')[0], seed);
-    toggleBracket(bracket);
+    bracket = LocBracket.split('-')[0];
+    setURL(bracket, seed);
+    setBracket();
     setCombatants();
+    addButtonCSS();
 }
 
 function addBracketsToPage() {
@@ -311,12 +344,11 @@ BRACKET_FORM.addEventListener('submit', (e) => {
         saveBracket();
     } else {
         e.preventDefault();
-        console.log('bracket not complete');
     }
 });
 
 function init() {
-    // addBracketsToPage();
+    addBracketsToPage();
     getDataFromURL();
     setBracket();
     setCombatants();
@@ -347,3 +379,5 @@ function init() {
 }
 
 init();
+
+
